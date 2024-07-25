b61cbcb545080806b8c28592350b30c79ad47a7a
Roman Khromets <rhromets@gmail.com>
2024-07-25 15:59:51 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mynotes_ffc_vandad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mynotes_ffc_vandad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text(
+          'Register',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('Register'),
+          ),
+        ],
+      ),
+    );
+  }
+}
