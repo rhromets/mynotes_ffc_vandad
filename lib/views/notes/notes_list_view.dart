8dc2f5b08a615962fcc6862bdee108b9938bde13
Roman Khromets <rhromets@gmail.com>
2024-08-05 19:39:52 +0300

    feat: migrate to firestore cloud db

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mynotes_ffc_vandad/services/crud/notes_service.dart';
+import 'package:mynotes_ffc_vandad/services/cloud/cloud_note.dart';
 import 'package:mynotes_ffc_vandad/utilities/dialogs/delete_dialog.dart';
 
-typedef NoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatelessWidget {
-  final List<DatabaseNote> notes;
+  final Iterable<CloudNote> notes;
   final NoteCallback onDeleteNote;
   final NoteCallback onTap;
 
@@ -21,7 +21,7 @@ class NotesListView extends StatelessWidget {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, index) {
-        var note = notes[index];
+        var note = notes.elementAt(index);
         return ListTile(
           onTap: () {
             onTap(note);
